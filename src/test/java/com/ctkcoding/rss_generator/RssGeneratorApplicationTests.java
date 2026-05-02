b63681726ba3a31d9d5ebfaa692e243e5d28d706
ctkcoding <ckateyiannis@gmail.com>
2026-05-01 22:43:56 -0500

    java gradle with basic health check

---
+++ src/test/java/com/ctkcoding/rss_generator/RssGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctkcoding.rss_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
