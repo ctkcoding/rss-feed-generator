4566b2e467ec04f0ceb517e5beeadc23243355ce
ctkcoding <ckateyiannis@gmail.com>
2026-05-14 22:06:14 -0500

    stub parse controller

---
+++ src/main/java/com/ctkcoding/rssgen/controller/ParseController.java
@@ -0,0 +1,13 @@
+package com.ctkcoding.rssgen.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@AllArgsConstructor
+public class ParseController {
+
+    // todo - manually trigger parse
+}
