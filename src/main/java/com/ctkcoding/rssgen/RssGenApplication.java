8972a50c686ec739fbade38769d69585f4263bb3
ctkcoding <ckateyiannis@gmail.com>
2026-05-14 21:18:48 -0500

    rename app and add health controller

---
+++ src/main/java/com/ctkcoding/rssgen/RssGenApplication.java
@@ -0,0 +1,13 @@
+package com.ctkcoding.rssgen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctkcoding/rssgen/RssGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctkcoding.rssgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RssGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
