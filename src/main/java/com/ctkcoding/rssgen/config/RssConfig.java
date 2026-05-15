144a150eee243e76017eed23ae4862e6c3aea5c9
ctkcoding <ckateyiannis@gmail.com>
2026-05-14 21:11:08 -0500

    add config properties

---
+++ src/main/java/com/ctkcoding/rssgen/config/RssConfig.java
@@ -0,0 +1,17 @@
+package com.ctkcoding.rssgen.config;
+
+public class RssConfig {
+    String pathRoot = System.getProperty("user.dir");
+    String artworkDir = "artwork";
+    String episodesDir = "episodes";
+    String infoDir = "info";
+
+    // todo - allow override by application.properties
+    String rssFileName = "rss.xml";
+    String showFileName = "show.json";
+    String episodeFileExtension = ".mp3";
+    String artworkFileExtension = ".jpeg";
+
+    Boolean extractArtwork = false;
+    Boolean fileWatch = false;
+}
