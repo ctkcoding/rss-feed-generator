e407ef5bb6ba3a9d2591b77a307626dd6817fe72
ctkcoding <ckateyiannis@gmail.com>
2026-05-14 22:09:25 -0500

    stub file controller

---
+++ src/main/java/com/ctkcoding/rssgen/controller/FileController.java
@@ -0,0 +1,47 @@
+package com.ctkcoding.rssgen.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@AllArgsConstructor
+public class FileController {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
+    @GetMapping("/rss")
+    public ResponseEntity<String> returnRss() {
+
+        // todo - return file if exists
+
+        // todo - return it as XML isntead of string
+        return ResponseEntity.ok("");
+    }
+
+    @GetMapping("/episode/{episode}")
+    public ResponseEntity<String> returnEpisode() {
+
+        // todo - return file if exists
+        // todo - find the file at /episodes/{episode}
+
+        // todo - return it as a file instead of string
+        return ResponseEntity.ok();
+    }
+
+    @GetMapping("/artwork/{artwork}")
+    public ResponseEntity<String> returnArtwork() {
+
+        // todo - return file if exists
+        // todo - find the file at /artwork/{artwork}
+
+        // todo - return it as a file instead of string
+        return ResponseEntity.ok();
+    }
+
+}
