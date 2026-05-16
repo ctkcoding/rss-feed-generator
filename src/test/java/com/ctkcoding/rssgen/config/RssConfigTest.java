e80c38daf6da92ea801f6ed8a4ee317e67c1c4ec
ctkcoding <ckateyiannis@gmail.com>
2026-05-15 19:40:46 -0500

    add default config test

--- src/test/java/com/ctkcoding/rssgen/config/RssConfigTest.java
+++ src/test/java/com/ctkcoding/rssgen/config/RssConfigTest.java
@@ -1,13 +1,29 @@
 package com.ctkcoding.rssgen.config;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
-public class RssConfigTest {
-    // todo - test defaults
+class RssConfigTest {
 
+    @Autowired
+    private RssConfig rssConfig;
 
-    // todo - test overrides
-
-
+    @Test
+    void defaults() {
+        assertEquals(System.getProperty("user.dir"), rssConfig.pathRoot);
+        assertEquals("artwork", rssConfig.artworkDir);
+        assertEquals("episodes", rssConfig.episodesDir);
+        assertEquals("info", rssConfig.infoDir);
+        assertEquals("rss.xml", rssConfig.rssFileName);
+        assertEquals("show.json", rssConfig.showFileName);
+        assertEquals(".mp3", rssConfig.episodeFileExtension);
+        assertEquals(".jpeg", rssConfig.artworkFileExtension);
+        assertEquals(false, rssConfig.extractArtwork);
+        assertEquals(false, rssConfig.fileWatch);
+        assertEquals("us-en", rssConfig.language);
+    }
 }
