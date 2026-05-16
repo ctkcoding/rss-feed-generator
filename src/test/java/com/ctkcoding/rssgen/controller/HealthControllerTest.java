75169ceaf480b947d220e6636144669b3db90178
ctkcoding <ckateyiannis@gmail.com>
2026-05-15 19:40:25 -0500

    add health controller test

--- src/test/java/com/ctkcoding/rssgen/controller/HealthControllerTest.java
+++ src/test/java/com/ctkcoding/rssgen/controller/HealthControllerTest.java
@@ -1,5 +1,26 @@
 package com.ctkcoding.rssgen.controller;
 
-public class HealthControllerTest {
-    // todo - test that API returns ok 200 and "Up and running"
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void healthCheck_returnsOk200AndUpAndRunning() throws Exception {
+        mockMvc.perform(get("/health"))
+            .andExpect(status().isOk())
+            .andExpect(content().string("Up and running"));
+    }
 }
