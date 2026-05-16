64ecb6c0c1dba568949594cd29c49012df1a0c7d
ctkcoding <ckateyiannis@gmail.com>
2026-05-16 08:13:27 -0500

    use fileservice in controller

--- src/main/java/com/ctkcoding/rssgen/controller/FileController.java
+++ src/main/java/com/ctkcoding/rssgen/controller/FileController.java
@@ -1,9 +1,12 @@
 package com.ctkcoding.rssgen.controller;
 
+import com.ctkcoding.rssgen.config.RssConfig;
+import com.ctkcoding.rssgen.service.FileService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +18,16 @@ public class FileController {
 
     private static final Logger logger = LoggerFactory.getLogger(FileController.class);
 
+    @Autowired
+    FileService fileService;
+
+    @Autowired
+    RssConfig rssConfig;
+
     @GetMapping("/rss")
     public ResponseEntity<String> returnRss() {
-
-        // todo - return file if exists
-
         // todo - return it as XML isntead of string
-        return ResponseEntity.ok("");
+        return ResponseEntity.ok(fileService.returnFileIfExists(rssConfig.getRssFileName()));
     }
 
     @GetMapping("/episode/{episode}")
