e4bdade5bc6b5fd5c4562bdf379d0d854c384ce5
ctkcoding <ckateyiannis@gmail.com>
2026-05-15 21:03:39 -0500

    update config properties and test

--- src/main/java/com/ctkcoding/rssgen/RssGenApplication.java
+++ src/main/java/com/ctkcoding/rssgen/RssGenApplication.java
@@ -2,8 +2,10 @@ package com.ctkcoding.rssgen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class RssGenApplication {
 
 	public static void main(String[] args) {
