308fee5075b20f45e1a6afdb3bfa58a41ea1b289
ctkcoding <ckateyiannis@gmail.com>
2026-06-06 14:41:04 -0500

    update tests after parse on startup

--- src/test/java/com/ctkcoding/rssgen/controller/HealthControllerTest.java
+++ src/test/java/com/ctkcoding/rssgen/controller/HealthControllerTest.java
@@ -8,9 +8,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 @AutoConfigureMockMvc
 class HealthControllerTest {
 
